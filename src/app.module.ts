031fa62a50de661feb8f23e56c31924450881039
Komal Rai <99144267+KomalRai18@users.noreply.github.com>
2026-04-06 18:59:04 +0500

    update app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,9 +13,11 @@ import { Driver } from './driver/entity/driver.entity';
 
 @Module({
   imports: [
+    AuthModule, DriverModule, RidesModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forFeature([Auth, Ride, Driver]),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         const databaseUrl = configService.get<string>('DATABASE_URL')
@@ -29,16 +31,18 @@ import { Driver } from './driver/entity/driver.entity';
           type: 'postgres',
           url: databaseUrl, // preferred
           // fallback if you want separate env vars
-          host: configService.get<string>('DB_HOST'),
-          port: parseInt(configService.get<string>('DB_PORT') || '5432'),
-          username: configService.get<string>('DB_USERNAME'),
-          password: configService.get<string>('DB_PASSWORD'),
-          database: configService.get<string>('DB_NAME'),
+          //host: configService.get<string>('DB_HOST'),
+          //port: parseInt(configService.get<string>('DB_PORT') || '5432'),
+          //username: configService.get<string>('DB_USERNAME'),
+          //password: configService.get<string>('DB_PASSWORD'),
+          //database: configService.get<string>('DB_NAME'),
           autoLoadEntities: true,
           synchronize: true, // ❗ turn OFF in production
-          logging: process.env.NODE_ENV !== 'production',
+          //logging: process.env.NODE_ENV !== 'production',
   
-          ssl: { rejectUnauthorized: false },
+          ssl: process.env.NODE_ENV === 'production' 
+          ? { rejectUnauthorized: false } 
+          : false,
           extra: {
               max: 5, // Reduced for connection pooler (less aggressive)
               min: 1,
